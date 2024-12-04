14a82d29b09ff032b1d55ed9da77c3a612c9462e
Bot1719744 <frankliu.TJ95@gmail.com>
2024-12-03 18:19:20 -0800

    Broken Version

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,37 +1,38 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
-
 export default function CourseRoutes(app) {
-    app.get("/api/courses", (req, res) => {
-        const courses = dao.findAllCourses();
-        res.send(courses);
+    // GET all courses
+    app.get("/api/courses", async (req, res) => {
+        const courses = await dao.findAllCourses(); // Retrieve courses
+        res.json(courses); // Send the courses list
     });
 
-    app.delete("/api/courses/:courseId", (req, res) => {
+    // DELETE a course
+    app.delete("/api/courses/:courseId", async (req, res) => {
         const { courseId } = req.params;
-        dao.deleteCourse(courseId);
-        res.sendStatus(204);
+        const status = await dao.deleteCourse(courseId);
+        res.send(status);
     });
 
-    app.put("/api/courses/:courseId", (req, res) => {
+        // PUT (update) a course
+    app.put("/api/courses/:courseId", async (req, res) => {
         const { courseId } = req.params;
         const courseUpdates = req.body;
-        dao.updateCourse(courseId, courseUpdates);
-        res.sendStatus(204);
+        const status = await dao.updateCourse(courseId, courseUpdates);
+        res.send(status);
     });
 
-    app.get("/api/courses/:courseId/modules", (req, res) => {
+    // GET modules for a course
+    app.get("/api/courses/:courseId/modules",  (req, res) => {
         const { courseId } = req.params;
         const modules = modulesDao.findModulesForCourse(courseId);
         res.json(modules);
     });
 
+    // POST a module for a course
     app.post("/api/courses/:courseId/modules", (req, res) => {
         const { courseId } = req.params;
-        const module = {
-            ...req.body,
-            course: courseId,
-        };
+        const module = { ...req.body, course: courseId };
         const newModule = modulesDao.createModule(module);
         res.send(newModule);
     });
--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -1,18 +1,26 @@
-import db from "../Database/index.js";
-let { users } = db;
+import model from "./model.js";
 
-export const createUser = (user) => {
-    user = {...user , _id: String(Date.now()) }
-    users = [...users, user];
-    return user;
+export const findAllUsers = () => model.find();
+export const findUserById = (userId) => model.findById(userId);
+export const findUserByUsername = (username) =>  model.findOne({ username: username });
+export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
+export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+export const findUsersByRole = (role) => model.find({ role: role });
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+        $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
 };
-export const findAllUsers = () => users;
-export const findUserById = (userId) => {users.find((user) => user._id === userId)};
-export const findUserByUsername = (username) => users.find((user) => user.username === username);
-export const findUserByCredentials = (username, password) =>{
-    return(users.find( (user) => user.username === username && user.password === password ))
+export const createUser = async (user) => {
+    try {
+        delete user._id; // Ensure `_id` is removed
+        return await model.create(user); // MongoDB will create `_id` automatically
+    } catch (error) {
+        console.error("Error in DAO createUser:", error); // Log the error
+        throw error; // Propagate the error to the route
+    }
 };
-export const updateUser = (userId, user) => {
-    (users = users.map((u) => (u._id === userId ? user : u)))
-};
-export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
+
+
